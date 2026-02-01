91cff8f3117589c6c148950393c5a53c7bb1eabe
eleuc <eleuterioc@gmail.com>
2026-02-01 12:16:04 -0500

    Mi primer menú funcional con Firebaseclear

---
+++ public/config.js
@@ -0,0 +1,13 @@
+// public/config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyB717Vc2v56WZkx9nNzHUuHRTQgQb8S-wk",
+  authDomain: "pedidos-jhoanes.firebaseapp.com",
+  projectId: "pedidos-jhoanes",
+  storageBucket: "pedidos-jhoanes.firebasestorage.app",
+  messagingSenderId: "1027681257600",
+  appId: "1:1027681257600:web:e0854891a0da511fbe3e65",
+  measurementId: "G-FSR8Z2XDY3"
+};       
+  
+  
+  
